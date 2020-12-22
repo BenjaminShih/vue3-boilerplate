95b5f91a8f0c26a52b084aded903961436dac4a2
sjn <shijunnan8688@dingtalk.com>
2020-12-22 10:59:16 +0800

    upd: 测试线上部署

--- src/utils/api/home.ts
+++ src/utils/api/home.ts
@@ -1,5 +1,6 @@
 import http from '../http'
 
 export function logout(): Promise<any> {
-    return http.get('/metadata/logout')
+    // return http.get('/metadata/logout')
+    return http.post('/api/admin/v1/logout', null)
 }
