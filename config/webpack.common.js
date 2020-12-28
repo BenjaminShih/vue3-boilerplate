5971bcf36cc8b25b7f804d04efe1baa86c9944bf
sjn <shijunnan8688@dingtalk.com>
2020-12-28 10:09:13 +0800

    feat: css-loader

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -78,6 +78,20 @@ const config = {
                 }
             }]
         }, {
+            test: /\.css/,
+            use: [
+                'style-loader',
+                'css-loader',
+                {
+                    loader: 'postcss-loader',
+                    options: {
+                        postcssOptions: {
+                            plugins: [require('autoprefixer')],
+                        },
+                    },
+                }
+            ]
+        }, {
             test: /\.scss/,
             use: [
                 'style-loader',
